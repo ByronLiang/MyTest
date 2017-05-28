9960a8b2e37d04785e5413c3495fc9cd7d9d1ca4
ByronLiang <liangzhaocong@163.com>
2017-05-28 09:58:02 +0800

    update some function to make program flexible.

--- user1/test.php
+++ user1/test.php
@@ -3,5 +3,10 @@ echo "Hello world";
 $a = ['apple', 'banana'];
 $b = ['watermelon', 'pear'];
 echo date('Y-m-d', time());
-
 array_diff($a, $b);
+for ($c = 0; $c < 4; $c++) {
+  echo "yooo";
+}
+function add($a, $b) {
+  echo "$a + $b"; 
+}
